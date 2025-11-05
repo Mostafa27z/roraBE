f6f6c8cb8b7b507cd5e27c9ef8fdf205823d69fe
unknown <aleslam56@gmail.com>
2025-11-05 12:40:22 +0200

    role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Modules\Auth\Models\Role; 
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //  Create roles if not exist
+        $roles = ['admin', 'client'];
 
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+        }
+
+        //  Example user
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->command->info('✅ Roles (admin, client) and test user created successfully.');
     }
 }
